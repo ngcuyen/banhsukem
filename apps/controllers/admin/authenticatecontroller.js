2eeeacc6d93b9c0b974cde75957fb0eaf442a403
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2025-03-05 09:00:25 +0700

    feat: 2 backend pages

--- apps/controllers/admin/authenticatecontroller.js
+++ apps/controllers/admin/authenticatecontroller.js
@@ -3,4 +3,10 @@ var router = express.Router();
 router.use('/login', function (req, res) {
 	res.render('admin/login.ejs');
 });
+router.use('/userManage', function (req, res) {
+	res.render('admin/userManage.ejs');
+});
+router.use('/widget', function (req, res) {
+	res.render('admin/widget.ejs');
+});
 module.exports = router;
--- apps/controllers/servicecontroller.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-router.use('/', function (req, res) {
-	res.render('services.ejs');
-});
-module.exports = router;
